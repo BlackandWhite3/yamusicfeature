17e854e7adf3f52d991f3776f251407a99568d9c
AF <rzzelenograd@gmail.com>
2020-10-21 12:37:49 +0300

    add fixed height for main vue app

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,30 @@
 <template>
-  <div id="nav">
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
+  <div :style="{ 'min-height': currentMinHeight + 'px' }">
+    <div id="nav">
+      <router-link to="/">Home</router-link> |
+      <router-link to="/about">About</router-link>
+    </div>
+    <router-view />
   </div>
-  <router-view />
 </template>
+
+<script>
+export default {
+  data() {
+    return {
+      currentMinHeight: 966
+    };
+  },
+  methods: {
+    updateMinHeight() {
+      this.currentMinHeight = window.innerHeight;
+    }
+  },
+  created() {
+    window.addEventListener("resize", this.updateMinHeight);
+  }
+};
+</script>
 
 <style lang="scss">
 #app {
@@ -13,6 +33,8 @@
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
+  //min-height: 966px;
+  min-width: 840px;
 }
 
 #nav {
@@ -26,5 +48,8 @@
       color: #42b983;
     }
   }
+}
+html {
+  background-color: #f6f5f3;
 }
 </style>
